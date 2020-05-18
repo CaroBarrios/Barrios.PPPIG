640e4b7c0e8522e3576ece3aa8833eb5d1d18da3
CaroBarrios <52497122+CaroBarrios@users.noreply.github.com>
2020-05-18 20:29:36 -0300

    Add files via upload

--- PrimerParcialProgramacion/Funciones.h
+++ PrimerParcialProgramacion/Funciones.h
@@ -1,7 +1,22 @@
 #ifndef Funciones_H_INCLUDED
 #define Funciones_H_INCLUDED
 
+#define PROCESADOR_LEN 20
+#define MARCA_LEN 20
+
+typedef struct
+{
+    int id;
+    char procesador [PROCESADOR_LEN];
+    char marca [MARCA_LEN];
+    int precio;
+
+}Notebook;
+
 int aplicarDescuento(int precioProducto);
 int contarCaracteres(char* list, char caracter);
+int ordenarNotebooks(Notebook* list,int len);
+int Notebook_print(Notebook* pElement);
+int printNotebooks(Notebook* list, int length);
 
 #endif // FUNCIONES_H_INCLUDED
